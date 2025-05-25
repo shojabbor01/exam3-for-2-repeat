1fd44906f1af1d8add7470d67d41ca6a2f4a9221
shojabbor01 <jabborovshojabbor@gmail.com>
2025-05-26 03:25:32 +0500

    tamom kadm hamai cruda

--- hamsafar/urls.py
+++ hamsafar/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import home_list, create_trip, create_user, create_companion
+from .views import home_list, create_trip, create_user, create_companion, update_user, update_trip, update_companion, delete_companion, delete_user, delete_trip
 
 urlpatterns = [
     path('',home_list, name='home'),
     path('create_user/', create_user, name='createuser'),
     path('create_trip/', create_trip, name='createtrip'),
     path('create_companion/', create_companion, name='createcompanion'),
+    path('update_user/<int:id>/', update_user, name='updateuser'),
+    path('update_trip/<int:id>/', update_trip, name='updatetrip'),
+    path('update_companion/<int:id>/', update_companion, name='updatecompanion'),
+    path('delete_companion/<int:id>/', delete_companion, name='deletecompanion'),
+    path('delete_trip/<int:id>/', delete_trip, name='deletetrip'),
+    path('delete_user/<int:id>/', delete_user, name='deleteuser'),
+
 ]
--- hamsafar/views.py
+++ hamsafar/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Trip, User, CompanionRequest
+from django.http import HttpResponse
 # Create your views here.
 
 def home_list(request):
@@ -48,4 +49,67 @@ def create_companion(request):
     return render(request, 'create_companion_list.html')
     
     
-    
+def update_user(request, id):
+    users = User.objects.filter(id=id).first()
+    if not users:
+        return HttpResponse("user not found", status=404)
+    if request.method == 'POST':
+        users.username = request.POST.get('username')
+        users.email = request.POST.get('email')
+        users.password = request.POST.get('password')
+        if request.FILES.get('image'):
+            users.image = request.FILES.get('image')
+        users.save()
+        return redirect('home')
+    return render(request, 'update_users.html', {'std': users})
+       
+def update_trip(request, id):
+    trips = Trip.objects.filter(id=id).first()
+    if not trips:
+        return HttpResponse("trips not found", status=404)
+    if request.method == "POST":
+        trips.start_location = request.POST.get('start_location')
+        trips.end_location = request.POST.get('end_location')
+        trips.date = request.POST.get('date')
+        trips.seats_available = request.POST.get('seats_available')
+        trips.description = request.POST.get('description')
+        trips.save()
+        return redirect('home')
+    return render(request, 'update_trips.html', {'std': trips})
+
+
+def update_companion(request, id):
+    companions = CompanionRequest.objects.filter(id = id).first()
+    if not companions:
+        return HttpResponse("trips not found", status=404)
+    if request.method == "POST":
+        companions.start_location = request.POST.get('start_location')
+        companions.end_location = request.POST.get('end_location')
+        companions.date = request.POST.get('date')
+        companions.description = request.POST.get('description')
+        companions.save()
+        return redirect('home')
+    return render(request, 'update_companions.html', {'std': companions})
+
+
+def delete_user(request, id):
+    users = User.objects.filter(id=id).first()
+    if not users:
+        return HttpResponse('user not found')
+    users.delete()
+    return redirect('home')
+
+def delete_trip(request, id):
+    trips = Trip.objects.filter(id=id).first()
+    if not trips:
+        return HttpResponse('trip not found')
+    trips.delete()
+    return redirect('home')
+
+def delete_companion(request, id):
+    companions = CompanionRequest.objects.filter(id=id).first()
+    if not companions:
+        return HttpResponse('companions not found')
+    companions.delete()
+    return redirect('home')
+
