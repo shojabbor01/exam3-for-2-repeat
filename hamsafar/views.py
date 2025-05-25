b372c80bb912c50106ab6fb07b9e853b0c814bfd
shojabbor01 <jabborovshojabbor@gmail.com>
2025-05-26 01:06:08 +0500

    second func

--- hamsafar/views.py
+++ hamsafar/views.py
@@ -7,3 +7,23 @@ def home_list(request):
     triphome = Trip.objects.all()
     companionrequest = CompanionRequest.objects.all()
     return render(request, 'home_list.html', {'users':userhome, 'trips':triphome, 'companions':companionrequest})
+
+def create_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        create_at = request.POST.get('create_at')
+        image = request.FILES.get('image')
+        if username and email and password and create_at and image:
+            User.objects.create(username = username, email = email, password = password, create_at = create_at, image = image)
+            return redirect('home')
+        return render(request, 'create_user_list.html')
+    return render(request, 'create_user_list.html')
+
+
+    
+
+        
+
+        
